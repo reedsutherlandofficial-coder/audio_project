4578010b5a363d30dcf8725e566d2eb7769ec377
reed <reedsutherlandofficial@gmail.com>
2026-04-20 09:05:34 -0400

    A.3: more basics added, gruvboc style, librosa imported.

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 from textual.app import App, ComposeResult
 from textual.containers import Vertical, Horizontal
-from textual.widgets import Button, Header, Footer, Static, ContentSwitcher, DirectoryTree, Collapsible, Switch
+from textual.widgets import Button, Header, Footer, Static, ContentSwitcher, DirectoryTree, Collapsible, Switch, Label
+from pathlib import Path
+from typing import Iterable
 
 
 
-
-
+class FilteredDirectoryTree(DirectoryTree):
+    def filter_paths(self, paths: Iterable[Path]) -> Iterable[Path]:
+        return [path for path in paths if not path.name.startswith(".")]
 
 class AudioDirectoryApp(App):
     CSS_PATH = "audioproject.tcss"
@@ -14,17 +17,15 @@ class AudioDirectoryApp(App):
                 ("d", "toggle_dark", "Toggle Dark Mode"),
                 ]
     
-    def show_welcome(self):
-        yield Welcome()
-
     def compose(self) -> ComposeResult:
         yield Header()
         yield Footer()
 
         with Horizontal(id="buttons"):
-            yield Button("WelcomeScreen", id="welcome_screen")
+            yield Button("Welcome", id="welcome_screen")
             yield Button("Directory", id="directory_picker")
             yield Button("File Format", id="file_format")
+            yield Button("Export", id ="save")
             
         with ContentSwitcher(initial="welcome_screen"):
             yield Static(
@@ -34,9 +35,16 @@ class AudioDirectoryApp(App):
                 id="welcome_screen")
             
             with Collapsible(title="Choose A Directory To Load", id="directory_picker"):
-                yield DirectoryTree("./")
+                yield FilteredDirectoryTree("~")
+                yield Button("Pick Directory", id="new_directory")
 
-            with Static("Choose Audio Format", id ="file_format"):
+                def on_file_selected(self) -> None:
+                    pass
+                def on_button_pressed(self) -> None:
+                    pass
+
+
+            with Label("Choose Audio Format", id ="file_format"):
                 yield Horizontal(
                     Static("WAV:    ", classes="label"),
                     Switch(animate=True),
@@ -53,13 +61,24 @@ class AudioDirectoryApp(App):
                     classes="container",
                 )
 
+            with Collapsible(title="Save to Path", id="save"):
+                yield FilteredDirectoryTree("~")
+                yield Button("Save", id="pressed_save")
 
+                def on_file_selected(self) -> None:
+                    pass
+                def on_button_pressed(self) -> None:
+                    pass
+
+    def on_mount(self) -> None:
+        self.theme = "gruvbox"
 
     def on_button_pressed(self, event:Button.Pressed) -> None:
         self.query_one(ContentSwitcher).current = event.button.id
 
-    def on_mount(self) -> None:
-        pass
+    #def on_button_pressed(self, event:Button.Pressed) -> None:
+        #self.query_one(FilteredDirectoryTree)
+    
 
         
         
